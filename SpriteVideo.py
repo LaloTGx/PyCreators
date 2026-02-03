887a79e7893b001fd345c8fb3914097e9f026b1e
LaloTGx <89763322+LaloTGx@users.noreply.github.com>
2026-02-03 13:23:12 -0600

    Update SpriteVideo
    Ahora muestra titulos y esta separado por saltos de linea, tambien de
    acuerdo al tamaño del lienzo puede detectar cuantos sprites existen (de
    manera horizontal) sin la necesidad e contarlos manualmente, ahora
    permite hacer videos en segundos, minutos y horas, casi la mayoria de
    las preguntas ya tienen una respuesta por default y por ultimo ya no es
    necesario escribir toda la ruta del archivo (en el caso de linux) ahora
    permite escribir desde "~/". :)

--- SpriteVideo.py
+++ SpriteVideo.py
@@ -16,6 +16,9 @@ Inputs interactivos:
 
 Dependencias:
     py -m pip install pillow imageio imageio-ffmpeg
+
+Instalacion(arch):
+    python -m pip install imageio imageio-ffmpeg pillow numpy
 """
 
 import imageio.v3 as iio
@@ -34,11 +37,17 @@ PRESETS = {
 
 def ask_path(prompt: str):
     while True:
-        p = Path(input(prompt).strip().strip('"'))
+        user_input = input(prompt).strip().strip('"')
+
+        if not user_input:
+            return None
+
+        p = Path(user_input).expanduser()
+
         if p.exists():
             return p
-        print("No encontré ese archivo. Intenta otra vez.")
 
+        print(f"No encontré ese archivo en {p} Intenta otra vez.")
 
 def ask_int(prompt: str, min_val=1, max_val=None, default=None):
     while True:
@@ -72,7 +81,20 @@ def ask_float(prompt: str, min_val=None, default=None):
         except ValueError:
             print("Ingresa un número (puede ser decimal).")
 
+def ask_time_format():
+    print(f"\n: -- Duracion del Video -- :")
+    print("[s] Segundos")
+    print("[m] Minutos")
+    print("[h] Horas")
+    choice = input("Seleccioné la opción (s/m/h) [s]: ").strip().lower()
+    if not choice: choice = 's'
+    val = ask_float("Cantidad", default=4.0)
 
+    if choice == 'm': return val * 60
+    if choice == 'h': return val * 3600
+    return val
+
+# Resolution
 def ask_resolution():
     txt = input("Resolución (720p/1080p/2k/4k o WxH) [1080p]: ").strip().lower()
     if not txt:
@@ -92,12 +114,10 @@ def ask_resolution():
     print("Entrada inválida, uso 1080p.")
     return PRESETS["1080p"]
 
-
+# Slice
 def slice_horizontal(sheet_img: Image.Image, total_frames: int):
     sw, sh = sheet_img.size
     frame_w = sw // total_frames
-    if frame_w * total_frames != sw:
-        print("⚠️ Advertencia: el ancho del sheet no divide exacto entre los frames; el último puede recortarse.")
     frames = []
     x = 0
     for i in range(total_frames):
@@ -107,7 +127,7 @@ def slice_horizontal(sheet_img: Image.Image, total_frames: int):
         x += frame_w
     return frames
 
-
+# Sprite Center
 def composite_center(frame: Image.Image, out_size, scale=1.0):
     W, H = out_size
 
@@ -125,23 +145,29 @@ def composite_center(frame: Image.Image, out_size, scale=1.0):
 
 
 def main():
-    print("=== Sprite -> Video (simple) ===")
+    print(f"\n: -- SpriteSheet > Video -- :")
+    print("# Para Salir Presione ENTER Sin Escribir Nada #")
+    print(f"\n: -- Propiedades del SpriteSheet -- :")
     sheet_path = ask_path("Ruta del sprite: ")
+
+    if sheet_path is None:
+        print("- Saliendo...")
+        return
 
     sheet = Image.open(sheet_path).convert("RGBA")
     sw, sh = sheet.size
-    print(f"Sprite sheet: {sw}x{sh}px")
+    print(f"- Sprite sheet: W:{sw}px H:{sh}px")
 
-    total_frames = ask_int("frameCount (número total de frames)", min_val=1)
+    canvas_size = ask_int("Tamaño del lienzo (px)", min_val=1, default=64 )
+    total_frames = sw // canvas_size
+    print(f"- Detectados: {total_frames} frames totales.")
+
+    print(f"\n: -- Propiedades de la Animación -- :")
     start_frame = ask_int("startFrame (1-based)", min_val=1, max_val=total_frames, default=1)
     end_frame = ask_int("endFrame (1-based)", min_val=start_frame, max_val=total_frames, default=total_frames)
-
     scale = ask_float("animationScale (1 = tamaño original)", min_val=0.01, default=1.0)
-
-    duration_s = ask_float("Duración total de la animación en segundos", min_val=0.001, default=4.0)
-
+    duration_s = ask_time_format()
     out_w, out_h = ask_resolution()
-
     all_frames = slice_horizontal(sheet, total_frames)
 
     s = start_frame - 1
@@ -151,7 +177,7 @@ def main():
 
     # Calcular fps según duración total deseada
     fps = used_count / duration_s
-    print(f"Generando video: {used_count} frames en {duration_s}s -> FPS ≈ {fps:.3f}")
+    print(f"- Generando video: {used_count} frames en {duration_s}s -> FPS ≈ {fps:.3f}")
 
     # Componer frames en canvas negro
     video_frames = []
@@ -164,10 +190,11 @@ def main():
     if out_txt:
         out_name = Path(out_txt if out_txt.lower().endswith(".mp4") else out_txt + ".mp4")
 
-    print("Renderizando (esto puede tardar un poco)...")
-    iio.imwrite(out_name, video_frames, fps=fps, codec="libx264", in_pixel_format="rgb24", out_pixel_format="yuv420p")
+    print("- Renderizando (esto puede tardar un poco)...\n")
+    # iio.imwrite(out_name, video_frames, fps=fps, codec="libx264", in_pixel_format="rgb24", out_pixel_format="yuv420p")
+    iio.imwrite(out_name, video_frames, fps=fps, codec="libx264", pixelformat="yuv420p")
 
-    print(f"Listo: {out_name.resolve()}")
+    print(f"\n- Listo: {out_name.resolve()}")
 
 
 if __name__ == "__main__":
